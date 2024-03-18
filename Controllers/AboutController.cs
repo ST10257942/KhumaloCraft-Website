b13a8d4dec2fd7d5b75da2ecc8939d77f09211c0
Ewald Pagel <ST10257942@vcconnect.edu.za>
2024-03-18 15:25:18 +0200

    Add project files.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KhumaloCraft.Controllers // Adjust namespace to match your project
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MyWorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KhumaloCraft.Controllers  // Adjust namespace to match your project
+{
+    public class MyWorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
